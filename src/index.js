513bf3fe30cd246875b8e8720a1cf0a84d471917
Siedemus <barsak1177@gmail.com>
2024-02-04 18:33:51 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("ready", () => console.log(`${client.user.tag} is ready!`));
+
+client.login(process.env.TOKEN);
