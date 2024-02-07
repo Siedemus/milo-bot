7627b018cd7004fd1d5330bca4daff7cc15c2325
b <barsak1177@gmail.com>
2024-02-07 19:46:10 +0100

    add downloading song and playing it

--- src/commands/muza.js
+++ src/commands/muza.js
@@ -1,5 +1,15 @@
-import { joinVoiceChannel } from "@discordjs/voice";
+import {
+  NoSubscriberBehavior,
+  createAudioPlayer,
+  createAudioResource,
+  joinVoiceChannel,
+} from "@discordjs/voice";
 import { SlashCommandBuilder } from "discord.js";
+import yts from "yt-search";
+import ytdl from "ytdl-core";
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 export default {
   data: new SlashCommandBuilder()
@@ -11,17 +21,58 @@ export default {
         .setDescription("podaj tytuł nutki wariacie")
         .setRequired(true)
     ),
-  execute(interaction) {
+  async execute(interaction) {
     const channelId = interaction.member.voice.channelId;
     const guildId = interaction.member.voice.guild.id;
     const adapterCreator = interaction.channel.guild.voiceAdapterCreator;
+    const input = interaction.options.getString("nuta");
+    const queueFolderPath = fileURLToPath(
+      path.join(dirname(import.meta.url), "\\queue")
+    );
 
-    const connection = joinVoiceChannel({
-      channelId,
-      guildId,
-      adapterCreator,
-    });
+    try {
+      const songInfo = await yts(input);
+      const {
+        url: songURL,
+        title: songTitle,
+        videoId: songID,
+      } = songInfo.all[0];
+      const songPath = `${queueFolderPath}\\${songID}.mp3`;
+      ytdl(songURL, { filter: "audioonly" })
+        .pipe(fs.createWriteStream(songPath))
+        .on("finish", () => {
+          const connection = joinVoiceChannel({
+            channelId,
+            guildId,
+            adapterCreator,
+          });
 
-    console.log(connection);
+          const player = createAudioPlayer({
+            behaviors: {
+              noSubscriber: NoSubscriberBehavior.Pause,
+            },
+          });
+
+          const resource = createAudioResource(songPath, {
+            inlineVolume: true,
+          });
+          resource.volume.setVolume(1);
+          player.play(resource);
+          connection.subscribe(player);
+
+          interaction.reply(`
+        # *RADIO WIELKIE C#CE.PL PRZEDSTAWIA:*
+        ## ${songTitle}
+        ## MIKSUJE DLA WAS DJ <@1203406103837278279> 
+        ### *${songURL}*
+        `);
+        });
+    } catch (e) {
+      interaction.reply({
+        content: "nie znalazłem takiej nutki wariacie",
+        ephemeral: true,
+      });
+      console.log(e);
+    }
   },
 };
