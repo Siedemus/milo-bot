b214499dfa516a65eae96858cf5de9fe5c8ae4fa
b <barsak1177@gmail.com>
2024-03-31 03:33:58 +0200

    add necessary types

--- src/utils/types/types.ts
+++ src/utils/types/types.ts
@@ -1,4 +1,12 @@
-import { Client, CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { AudioPlayer, VoiceConnection } from "@discordjs/voice";
+import {
+  Client,
+  CommandInteraction,
+  SlashCommandBuilder,
+  VoiceState,
+} from "discord.js";
+import fs from "fs";
+import yts from "yt-search";
 
 export interface Command {
   default: {
@@ -10,4 +18,40 @@ export interface Command {
 export interface CommandsURLAndPATH {
   commandsURL: string;
   commandsPATH: string;
+}
+
+export type VoiceExtendedCommandInteraction = CommandInteraction & VoiceState;
+
+export interface Song {
+  title: string;
+  url: string;
+  songPath: string;
+}
+
+export interface GuildQueue {
+  guildId: string;
+  connection: VoiceConnection;
+  player: AudioPlayer;
+  songQueue: Song[];
+  loop: boolean;
+}
+
+export type ExtendedSearchResult =
+  | yts.VideoSearchResult
+  | yts.LiveSearchResult
+  | yts.PlaylistSearchResult
+  | yts.ChannelSearchResult;
+
+export interface EmbedModifier {
+  [key: string]: any;
+  color?: string;
+  title?: string;
+  url?: string;
+  author?: { name: string; iconURL: string };
+  description?: string;
+  thumbnail?: string;
+  fields?: { name: string; value: string; inline?: string }[];
+  image?: string;
+  timeStamp?: true;
+  footer?: { text: string; iconURL: string };
 }
