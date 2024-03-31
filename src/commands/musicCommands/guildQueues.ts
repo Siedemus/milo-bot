0264a715865a004d8818e170537f316a3dbcb2ee
b <barsak1177@gmail.com>
2024-03-31 03:34:41 +0200

    add and initialize guildQueues object

---
+++ src/commands/musicCommands/guildQueues.ts
@@ -0,0 +1,140 @@
+import { AudioPlayer, VoiceConnection } from "@discordjs/voice";
+import { GuildQueue, Song } from "../../utils/types/types";
+
+class guildQueues {
+  private _guildQueues: GuildQueue[];
+
+  constructor() {
+    this._guildQueues = [];
+  }
+
+  private getGuildQueue(guildId: string): GuildQueue | undefined {
+    return this._guildQueues.find(
+      (guildQueue) => guildQueue.guildId === guildId
+    );
+  }
+
+  public createGuildQueue(
+    guildId: string,
+    connection: VoiceConnection,
+    player: AudioPlayer
+  ) {
+    const guildQueue = this.getGuildQueue(guildId);
+
+    if (!guildQueue) {
+      this._guildQueues.push({
+        guildId,
+        connection,
+        player,
+        songQueue: [],
+        loop: false,
+      });
+    } else {
+      throw new Error(
+        "You're trying to create guild queue that already exists."
+      );
+    }
+  }
+
+  public removeSongFromGuildSongQueue(guildId: string) {
+    const guildQueue = this.getGuildQueue(guildId);
+
+    if (guildQueue) {
+      if (guildQueue.songQueue.length > 0) {
+        guildQueue.songQueue.shift();
+      } else {
+        throw new Error(
+          "You're trying to remove a song from guild queue that is empty."
+        );
+      }
+    } else {
+      throw new Error(
+        "You're trying to remove a song from guild queue that not exist."
+      );
+    }
+  }
+
+  public addSongToGuildSongQueue(guildId: string, song: Song) {
+    const guildQueue = this.getGuildQueue(guildId);
+
+    if (guildQueue) {
+      guildQueue.songQueue.push(song);
+    } else {
+      throw new Error(
+        "You're trying to add a song to guild queue that not exist."
+      );
+    }
+  }
+
+  public changeGuildQueueLoopStatus(guildId: string) {
+    const guildQueue = this.getGuildQueue(guildId);
+
+    if (guildQueue) {
+      guildQueue.loop = !guildQueue.loop;
+    } else {
+      throw new Error(
+        "You're trying to change loop status of guild queue that not exist."
+      );
+    }
+  }
+
+  public getGuildQueueLoopStatus(guildId: string) {
+    const guildQueue = this.getGuildQueue(guildId);
+
+    return guildQueue?.loop;
+  }
+
+  public getGuildSongQueue(guildId: string) {
+    const guildQueue = this.getGuildQueue(guildId);
+
+    return guildQueue?.songQueue;
+  }
+
+  public setGuildQueuePlayer(guildId: string, audioPlayer: AudioPlayer) {
+    const guildQueue = this.getGuildQueue(guildId);
+
+    if (guildQueue) {
+      guildQueue.player = audioPlayer;
+    } else {
+      throw new Error(
+        "You're trying to set audio player of guild queue that not exist."
+      );
+    }
+  }
+
+  public getGuildQueuePlayer(guildId: string) {
+    const guildQueue = this.getGuildQueue(guildId);
+
+    return guildQueue?.player;
+  }
+
+  public setGuildQueueConnection(guildId: string, connection: VoiceConnection) {
+    const guildQueue = this.getGuildQueue(guildId);
+
+    if (guildQueue) {
+      guildQueue.connection = connection;
+    } else {
+      throw new Error(
+        "You're trying to set connection of guild queue that not exist."
+      );
+    }
+  }
+
+  public getGuildQueueConnection(guildId: string) {
+    const guildQueue = this.getGuildQueue(guildId);
+
+    return guildQueue?.connection;
+  }
+
+  public guildQueueExists(guildId: string): boolean {
+    const guildQueue = this.getGuildQueue(guildId);
+
+    if (guildQueue) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+export default new guildQueues();
