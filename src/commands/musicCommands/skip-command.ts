6c4aacffe7308a87f9deeed44471a47c7a8d2422
b <barsak1177@gmail.com>
2024-04-01 03:05:50 +0200

    hanle skip command

---
+++ src/commands/musicCommands/skip-command.ts
@@ -0,0 +1,65 @@
+import { SlashCommandBuilder } from "discord.js";
+import { Song, VoiceExtendedCommandInteraction } from "../../utils/types/types";
+import guildQueues from "./guildQueues.js";
+import { infoEmbed, skipEmebed } from "./musicEmbeds";
+import { createAudioResource } from "@discordjs/voice";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("skip")
+    .setDescription("Skip current song."),
+  execute: async (interaction: VoiceExtendedCommandInteraction) => {
+    const channelId = interaction.member?.voice.channelId;
+
+    if (!channelId) {
+      return await replyWithProblemInfo(
+        interaction,
+        "You're not on the voice channel."
+      );
+    }
+
+    const guildId = interaction.guildId!;
+    const songQueue = guildQueues.getGuildSongQueue(guildId);
+    const songQueueLength = songQueue?.length;
+
+    if (!songQueue || songQueueLength === 0) {
+      return await replyWithProblemInfo(
+        interaction,
+        "There's nothing to skip."
+      );
+    }
+
+    handleSongSkip(guildId, songQueue, interaction);
+  },
+};
+
+const replyWithProblemInfo = async (
+  interaction: VoiceExtendedCommandInteraction,
+  content: string
+) => {
+  await interaction.reply({
+    embeds: [infoEmbed.setDescription(content)],
+    ephemeral: true,
+  });
+};
+
+const handleSongSkip = async (
+  guildId: string,
+  songQueue: Song[],
+  interaction: VoiceExtendedCommandInteraction
+) => {
+  const guildQueuePlayer = guildQueues.getGuildQueuePlayer(guildId)!;
+  const currentSongTitle = songQueue[0].title;
+
+  guildQueuePlayer.stop();
+  await replyWithSkipInfo(currentSongTitle, interaction);
+};
+
+const replyWithSkipInfo = async (
+  currentSongTitle: string,
+  interaction: VoiceExtendedCommandInteraction
+) => {
+  await interaction.reply({
+    embeds: [skipEmebed.setTitle(`Successfully skipped: ${currentSongTitle}`)],
+  });
+};
